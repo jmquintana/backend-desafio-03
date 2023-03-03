cdd38d38079ce1aa2a58158b5d5cddc9d77ca9ac
José <mailjmq@gmail.com>
2023-03-03 01:03:20 -0300

    limit and id param running and ready to deliver

--- src/app.js
+++ src/app.js
@@ -1,35 +1,9 @@
 import ProductManager from "./ProductManager.js";
-import express, { query } from "express";
+import express from "express";
 
 const app = express();
 const manager = new ProductManager();
 const products = await manager.getProducts();
-
-// Ejemplo: consulta en express
-app.get("/saludo", (req, res) => {
-	res.send("Hola a todos, pero ahora desde express");
-});
-
-app.get("/bienvenida", (req, res) => {
-	res.setHeader("Content-type", "text/html");
-	res.send("<p style='color:blue'>Bienvenida!<p>");
-});
-
-app.get("/usuario", (req, res) => {
-	res.setHeader("Content-Type", "application/json");
-	res.end(
-		JSON.stringify(
-			{
-				nombre: "Alfredo",
-				apellido: "Mercurio",
-				edad: 60,
-				correo: "alfredo.mercurio@gmail.com",
-			},
-			null,
-			3
-		)
-	);
-});
 
 app.get("/products/", (req, res) => {
 	res.setHeader("Content-Type", "application/json");
